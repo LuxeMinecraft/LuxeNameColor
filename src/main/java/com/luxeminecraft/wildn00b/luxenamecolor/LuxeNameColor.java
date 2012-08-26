7320d7e381f61fdc013f71f7639317630cb5e73a
Dan Printzell <wildn00b@softwepz.co.cc>
2012-08-26 20:28:43 +0200

    + Added project base
    Signed-off-by: Dan Printzell <wildn00b@softwepz.co.cc>

---
+++ src/main/java/com/luxeminecraft/wildn00b/luxenamecolor/LuxeNameColor.java
@@ -0,0 +1,17 @@
+package com.luxeminecraft.wildn00b.luxenamecolor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LuxeNameColor extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+}
